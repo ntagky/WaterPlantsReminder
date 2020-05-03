bfec0f8a2ee4f5b499a49558a761b10e0a4a32bb
exJett <ntagkonik@csd.auth.gr>
2020-05-03 20:29:07 +0300

    Initial commit

---
+++ app/src/main/java/gr/csd/plantsreminder/MainActivity.java
@@ -0,0 +1,113 @@
+package gr.csd.plantsreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager slideViewPager;
+    private LinearLayout linearBottomLayout;
+    private Button backButton;
+    private Button nextButton;
+    private int currentPage = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        slideViewPager = findViewById(R.id.slideViewPager);
+        SliderAdapter sliderAdapter = new SliderAdapter(this);
+        slideViewPager.setAdapter(sliderAdapter);
+        slideViewPager.addOnPageChangeListener(onPageChangeListener);
+
+        linearBottomLayout = findViewById(R.id.linearBottomLayout);
+        addDotsIndicators(0);
+
+        nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (nextButton.getText().equals(getString(R.string.finish))){
+                    changeIntent();
+                } else
+                    slideViewPager.setCurrentItem(currentPage + 1);
+
+            }
+        });
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                slideViewPager.setCurrentItem(currentPage - 1);
+
+            }
+        });
+    }
+
+    private void changeIntent() {
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    private void addDotsIndicators(int position){
+        TextView[] dotsTextView = new TextView[3];
+        linearBottomLayout.removeAllViews();
+
+        for (int i = 0; i< dotsTextView.length; i++){
+
+            dotsTextView[i] = new TextView(this);
+            dotsTextView[i].setText(Html.fromHtml("&#8226;"));
+            dotsTextView[i].setTextSize(35);
+            dotsTextView[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            linearBottomLayout.addView(dotsTextView[i]);
+
+        }
+
+        dotsTextView[position].setTextColor(getResources().getColor(R.color.colorWhite));
+    }
+
+    private ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicators(position);
+
+            currentPage = position;
+
+            if (position == 0){
+                backButton.setVisibility(View.INVISIBLE);
+                nextButton.setText(R.string.next);
+            }else if (position == 1){
+                backButton.setVisibility(View.VISIBLE);
+                nextButton.setText(R.string.next);
+            }else if (position == 2){
+                nextButton.setText(R.string.finish);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+}
---
+++ app/src/main/java/gr/csd/plantsreminder/SliderAdapter.java
@@ -0,0 +1,64 @@
+package gr.csd.plantsreminder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] linearSlidesLayout = { R.layout.slider_welcome_layout, R.layout.slider_login_layout, R.layout.slider_add_plant };
+
+    private RecyclerView recyclerView;
+
+    public SliderAdapter(){}
+
+    public SliderAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(linearSlidesLayout[position], container, false);
+
+        if (position == 1){
+
+
+        }else if (position == 2){
+
+
+
+        }
+
+        container.addView(view);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout) object);
+    }
+
+}
