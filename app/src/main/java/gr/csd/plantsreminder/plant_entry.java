45849148247dde0394f8236f8141f0a9a9ab499e
Nef <marianefn@csd.auth.gr>
2020-05-20 20:44:45 +0300

    plant entry(check the align)

---
+++ app/src/main/java/gr/csd/plantsreminder/plant_entry.java
@@ -0,0 +1,14 @@
+package gr.csd.plantsreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class plant_entry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plant_entry);
+    }
+}
